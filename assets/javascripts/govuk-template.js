08a2828ad8bfabfd322cb6ae361e8a8eb38322e5
michaeldfallen <michaeldfallen@gmail.com>
2013-08-12 17:28:08 +0100

    deploying Govuk Play templates 0.0.4

---
+++ assets/javascripts/govuk-template.js
@@ -0,0 +1,172 @@
+$(function() {
+  if (typeof window.GOVUK === 'undefined') {
+    window.GOVUK = {};
+  }
+
+  GOVUK.addCookieMessage = function () {
+    var $message = $('#global-cookie-message'),
+        hasCookieMessage = ($message.length && getCookie('seen_cookie_message') === null);
+
+    if (hasCookieMessage) {
+      $message.show();
+      setCookie('seen_cookie_message', 'yes', 28);
+    }
+  };
+});
+//Reusable functions
+var Alphagov = {
+  daysInMsec: function(d) {
+    return d * 24 * 60 * 60 * 1000;
+  },
+  cookie_domain: function() {
+    var host_parts = document.location.host.split(':')[0].split('.').slice(-3);
+    return '.' + host_parts.join('.');
+  },
+  read_cookie: function(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = jQuery.trim(cookies[i]);
+        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  },
+  delete_cookie: function(name) {
+    if (document.cookie && document.cookie !== '') {
+      var date = new Date();
+      date.setTime(date.getTime() - Alphagov.daysInMsec(1)); // 1 day ago
+      document.cookie = name + "=; expires=" + date.toGMTString() + "; domain=" + Alphagov.cookie_domain() + "; path=/";
+    }
+  },
+  write_cookie: function(name, value) {
+    var date = new Date();
+    date.setTime(date.getTime() + Alphagov.daysInMsec(4 * 30)); // 4 nominal 30-day months in the future
+    document.cookie = name + "=" + encodeURIComponent(value) + "; expires=" + date.toGMTString() + "; domain=" + Alphagov.cookie_domain() + "; path=/";
+  }
+};
+
+function recordOutboundLink(e) {
+  _gat._getTrackerByName()._trackEvent(this.href, 'Outbound Links');
+  setTimeout('document.location = "' + this.href + '"', 100);
+  return false;
+}
+
+$(document).ready(function() {
+  $('.print-link a').attr('target', '_blank');
+
+  // header search toggle
+  $('.js-header-toggle').on('click', function(e) {
+    e.preventDefault();
+    $($(e.target).attr('href')).toggleClass('js-visible');
+    $(this).toggleClass('js-hidden');
+  });
+
+  var $searchFocus = $('.js-search-focus');
+  $searchFocus.each(function(i, el){
+    if($(el).val() !== ''){
+      $(el).addClass('focus');
+    }
+  });
+  $searchFocus.on('focus', function(e){
+    $(e.target).addClass('focus');
+  });
+  $searchFocus.on('blur', function(e){
+    if($(e.target).val() === ''){
+      $(e.target).removeClass('focus');
+    }
+  });
+
+  $("nav").delegate('a', 'click', function(){
+    var hash;
+    var href = $(this).attr('href');
+    if(href.charAt(0) === '#'){
+      hash = href;
+    }
+    else if(href.indexOf("#") > 0){
+      hash = "#" + href.split("#")[1];
+    }
+    if($(hash).length == 1){
+      $("html, body").animate({scrollTop: $(hash).offset().top - $("#global-header").height()},10);
+    }
+  });
+
+  // hover, active and focus states for buttons in IE<8
+  if ($.browser.msie && $.browser.version < 8) {
+    $('.button').not('a')
+      .on('click focus', function (e) {
+        $(this).addClass('button-active');
+      })
+      .on('blur', function (e) {
+        $(this).removeClass('button-active');
+      });
+
+    $('.button')
+      .on('mouseover', function (e) {
+        $(this).addClass('button-hover');
+      })
+      .on('mouseout', function (e) {
+        $(this).removeClass('button-hover');
+      });
+  }
+
+  // fix for printing bug in Windows Safari
+  (function () {
+    var windows_safari = (window.navigator.userAgent.match(/(\(Windows[\s\w\.]+\))[\/\(\s\w\.\,\)]+(Version\/[\d\.]+)\s(Safari\/[\d\.]+)/) !== null),
+        $new_styles;
+
+    if (windows_safari) {
+      // set the New Transport font to Arial for printing
+      $new_styles = $("<style type='text/css' media='print'>" +
+                      "@font-face {" +
+                      "font-family: nta !important;" +
+                      "src: local('Arial') !important;" +
+                      "}" +
+                      "</style>");
+      document.getElementsByTagName('head')[0].appendChild($new_styles[0]);
+    }
+  }());
+
+  if (window.GOVUK) {
+    if (GOVUK.addCookieMessage) {
+      GOVUK.addCookieMessage();
+    }
+  }
+});
+
+
+function setCookie(name, value, days){
+  var cookieString = name + "=" + value + "; path=/";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    cookieString = cookieString + "; expires=" + date.toGMTString();
+  }
+  if (document.location.protocol == 'https:'){
+    cookieString = cookieString + "; Secure";
+  }
+  document.cookie = cookieString;
+}
+
+function getCookie(name){
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0, len = ca.length; i < len; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1, c.length);
+    }
+    if (c.indexOf(nameEQ) === 0) {
+      return c.substring(nameEQ.length, c.length);
+    }
+  }
+  return null;
+}
+;
+
+
+
